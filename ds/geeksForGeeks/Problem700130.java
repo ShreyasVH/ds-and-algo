e06652fa71a619e3d349e463df1358827d48392f
ShreyasVH <shreyas.deve.1@gmail.com>
2026-02-02 19:52:10 +0530

    problem of the day

---
+++ ds/geeksForGeeks/Problem700130.java
@@ -0,0 +1,105 @@
+/*
+Split a Linked List into two halves
+
+Given a Circular linked list. The task is split into two Circular Linked lists. If there are an odd number of nodes in the given circular linked list then out of the resulting two halved lists, the first list should have one node more than the second list.
+
+Examples :
+
+Input: LinkedList : 10->4->9
+Output: 10->4 , 9
+
+Explanation: After dividing linked list into 2 parts , the first part contains 10, 4 and second part contain only 9.
+Input: LinkedList : 10->4->9->10
+Output: 10->4 , 9->10
+
+Explanation: After dividing linked list into 2 parts , the first part contains 10, 4 and second part contain 9, 10.
+Constraints:
+2 <= number of nodes <= 10^5
+1 <= node->data <= 10^3
+
+*/
+
+package ds.geeksForGeeks;
+
+import java.util.*;
+import utils.*;
+import java.lang.reflect.Constructor;
+
+public class Problem700130
+{
+	public static void main(String args[]) throws Exception
+	{
+
+		Class<?> clazz = new Object() {}.getClass().getEnclosingClass();
+
+        Constructor<?> constructor = clazz.getDeclaredConstructor();
+        Object problem = constructor.newInstance();
+
+        LinkedListNode head = new LinkedListNode(10);
+        head.next = new LinkedListNode(4);
+        head.next.next = new LinkedListNode(9);
+        head.next.next.next = head;
+        LinkedListUtils.printCircularList(head);
+        Pair<LinkedListNode, LinkedListNode> pair = (Pair<LinkedListNode, LinkedListNode>) clazz.getMethod("splitList", LinkedListNode.class).invoke(problem, head);
+        LinkedListUtils.printCircularList(pair.first);
+        LinkedListUtils.printCircularList(pair.second);
+
+        System.out.println("-------------------------------------------------------");
+
+        head = new LinkedListNode(10);
+        head.next = new LinkedListNode(4);
+        head.next.next = new LinkedListNode(9);
+        head.next.next.next = new LinkedListNode(8);
+        head.next.next.next.next = head;
+        LinkedListUtils.printCircularList(head);
+        pair = (Pair<LinkedListNode, LinkedListNode>) clazz.getMethod("splitList", LinkedListNode.class).invoke(problem, head);
+        LinkedListUtils.printCircularList(pair.first);
+        LinkedListUtils.printCircularList(pair.second);
+	}
+
+    public Pair<LinkedListNode, LinkedListNode> splitList(LinkedListNode head) {
+        int index = 0;
+
+        LinkedListNode curr = head;
+        while(curr.next != head)
+        {
+            curr = curr.next;
+            index++;
+        }
+
+        int length = index + 1;
+
+        int middle = (length + 1) / 2;
+
+        LinkedListNode node = head;
+        for(int i = 1; i < middle; i++)
+        {
+            node = node.next;
+        }
+
+        LinkedListNode second = node.next;
+        node.next = head;
+        curr.next = second;
+
+        return new Pair<>(head, second);
+
+    }
+
+    class Pair<F, S>
+    {
+        F first;
+        S second;
+
+        Pair(F first, S second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+    }
+}
+
+
+/*
+Time complexity: O(n)
+Space complexity: O(1)
+*/
